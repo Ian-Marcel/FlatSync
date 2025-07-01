60f2d147f1cd8bc8c668de5033cb1cbcf4f8c7ed
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-07-01 04:09:00 -0300

    04:09:00 - 01/07/2025

--- flatsyncd/app/source/git.sh
+++ flatsyncd/app/source/git.sh
@@ -1,22 +1,86 @@
 #!/usr/bin/env bash
 
 # git config set --local
-import data/git
+import git_config_default_values
+
 if [ -d "$FLATSYNC_GIT"/.git ]; then
 	cd "$FLATSYNC_GIT"
-	for index in "${!git_preference[@]}"; do
-		if ! git config get --local "${git_preference[$index]}"; then
-			git config set --local "${git_preference[$index]}" "${git_preference_value[$index]}"
-		else
-			continue
+	if ! [ -e remote_flatpaks ]; then
+		printf "Remote list not found! Is this you're first sync?"
+		while true ; do
+			read -rp "(Y)es | (N)o : " NO_LIST
+			if [ "$NO_LIST" = Y ] || [ "$NO_LIST" = Yes ]; then
+				flatpak list --columns=application --app |
+					tee -p remote_flatpaks &>/dev/null
+				git add remote_flatpaks
+				git commit -q -m "$(date "%H:%M:%S - %d/%m/%Y")"
+				git push -q
+				exit 0
+				break
+			elif [ "$NO_LIST" = N ] || [ "$NO_LIST" = No ]; then
+				git fetch -q
+				git pull -q
+				break
+			else
+				printf "Wrong answer, type either Yes or No! \n"
+			fi
+		done
+	else
+		git fetch -q
+		git pull -q
+	fi
+else
+	cd "$FLATSYNC_GIT"
+	printf "Oh oh, sync repository not found! \nIs this you're first sync or would you like to import a sync repository?\n "
+	while true; do
+		read -rp "create(1) | import(2): " NO_REPO
+		if [ "$NO_REPO" = 1 ]; then
+			git init
+			ssh-keygen -t ed25519 -f "$HOME"/.ssh/flatsync_key -q -N "" -C "SSH key for Flatsync - a synchronizer for flatpak applications"
+			for index in "${!git_preference[@]}"; do
+				git config set --local "${git_preference[$index]}" "${git_preference_value[$index]}"
+			done
+			printf "Copy and paste the line bellow to your cloud git service:\n$(<"$HOME"/.ssh/flatsync_key.pub) \n"
+			printf "\rPlease provide the repository url, make sure it is for SSH! \n"
+			#while true; do
+			read -rp "URL: " NR_GIT_ORIGIN
+			# create: git_url_regex_funtion
+			git remote add origin "$NR_GIT_ORIGIN" 
+			#done
+			break
+		elif [ "$NO_REPO" = 2 ]; then
+			printf "Ok..."
 			sleep 1s
+			break
+		else
+			printf "Wrong answer, type either 1 or 2! \n"
 		fi
 	done
-else
-	printf "Oh oh\n"
-	cd "$FLATSYNC_GIT"
-	git init
+	if [ "$NO_REPO" = 1 ]; then
+			flatpak list --columns=application --app |
+				tee -p remote_flatpaks &>/dev/null
+			git add remote_flatpaks
+			git commit -q -m "$(date "%H:%M:%S - %d/%m/%Y")"
+			git push -q
+			exit 0;
+	elif [ "$NO_REPO" = 2 ]; then
+			printf "\rPlease provide the repository url, make sure it is for SSH! \n"
+		#while true; do
+			read -rp "URL: " NR_GIT_ORIGIN
+			# create: git_url_regex_funtion
+			git clone "$NR_GIT_ORIGIN" "$FLATSYNC_GIT"/
+		#done
+		for index in "${!git_preference[@]}"; do
+			if ! git config get --local "${git_preference[$index]}"; then
+				git config set --local "${git_preference[$index]}" "${git_preference_value[$index]}"
+			else
+				continue
+				sleep 1s
+			fi
+		done
+	fi
 fi
+cd "$FLATSYNC_ROOT"
 
 # git config set --local user.name 'flatsync'
 # git config set --local user.email 'flatsync@fake.mail'
@@ -28,5 +92,3 @@ fi
 # git config set --local gpg.format ssh
 # git config set --local commit.gpgsign true
 
-# the following if statement works(it will print "Not found!" if gpg.format is unset): `if ! git config get --local gpg.format &> /dev/null ; then printf "Not found! \n";fi`
-# if var=Hello, `printf "\"$var"\"` will print "Hello"(with the commas)
